04ff26c964e66f95487d566332e39494aafaa01e
André Damázio <ferunosxd@gmail.com>
2018-09-16 14:52:53 -0300

    Update RssAdapter.kt

--- RSS/app/src/main/java/br/ufpe/cin/if710/rss/RssAdapter.kt
+++ RSS/app/src/main/java/br/ufpe/cin/if710/rss/RssAdapter.kt
@@ -12,15 +12,17 @@ import android.net.Uri
 
 
 class RssAdapter(private val noticias : List<ItemRSS>, private val c : Context): RecyclerView.Adapter<RssAdapter.ViewHolder> (){
-
+    //Cria novas views invocado pelo Layoutmanager
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : ViewHolder{
         val view = LayoutInflater.from(c).inflate(R.layout.itemlista,parent,false)
         return ViewHolder(view)
     }
+    //O viewholder fornece acesso as referencias de todos os itens da view.
     class ViewHolder(val view: View) : RecyclerView.ViewHolder(view){
         val rss_titulo = view.item_titulo
         val rss_data = view.item_data
     }
+    //Troca o conteudo de uma view de acordo com o layoutmanager
     override fun onBindViewHolder(holder: ViewHolder, position: Int){
         holder.rss_titulo.text = noticias[position].title
         holder.rss_data.text = noticias[position].pubDate
@@ -30,6 +32,7 @@ class RssAdapter(private val noticias : List<ItemRSS>, private val c : Context):
             startActivity(c,browserIntent,null)
         }
     }
+    //Metodo abstrato que retorna o tamanho do conjunto de dados
     override fun getItemCount(): Int {
         return noticias.size
     }
